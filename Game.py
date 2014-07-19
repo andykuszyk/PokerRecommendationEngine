e8cdd09457230af2fe5910b5917d00a2260e65ad
andy <andy@oldbanger>
2014-07-19 15:50:24 +0100

    Added Game

---
+++ Game.py
@@ -0,0 +1,14 @@
+class Game:
+    
+    self._numberOfPlayers = 0
+    self._bigBlind = 0
+    self._smallBlind = 0
+    
+    
+    def __init__(self,numberOfPlayers):
+        self._numberOfPlayers = numberOfPlayers
+    
+        self._hand = {card1, card2, card3}
+        
+    def GetNumberOfPlayers(self):
+        return _numberOfPlayers
